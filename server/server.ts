f4ca5baa580a94cf8207b3ba64c9c182c4dab39a
Sam <sam.k.phillips@gmail.com>
2022-03-15 22:58:54 -0400

    Blocking out the server

---
+++ server/server.ts
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+
+const app = express()
+
+// const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use('/api', AppRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
