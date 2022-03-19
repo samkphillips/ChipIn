b2a87114fc5223e1b21b4d8779ba77dcfcd1b864
Sam <sam.k.phillips@gmail.com>
2022-03-19 02:10:23 -0400

    Setting up routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
 import './styles/App.css'
 
 function App() {
+  //
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hi Mom!
-        </p>
+      <header>
+        {/* <Nav /> */}
+        <p>Howdy</p>
       </header>
+
+      <main>
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home />}
+          />
+          <Route 
+            path="/aboutus"
+            element={<AboutUs />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/App.css'
+
+function Home(props: any) {
+  return (
+    <div>
+      Home page!
+    </div>
+  )
+}
+
+export default Home
